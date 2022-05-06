c69cd24722afabfe0ecc726b57a87895cae8635e
Nicholas <appledev.booker@gmail.com>
2022-05-06 16:53:10 +0900

    feat: 일시정지 및 재시작 기능 구현

---
+++ Timer/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Timer
+//
+//  Created by Eunsoo KIM on 2022/05/06.
+//
+
+import Foundation
+
+enum K {
+    enum ButtonTitle {
+        static let start = "START"
+        static let pause = "PAUSE"
+        static let restart = "RESTART"
+    }
+    
+}
--- Timer/TimerViewController.swift
+++ Timer/TimerViewController.swift
@@ -12,9 +12,10 @@ final class TimerViewController: UIViewController {
     
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var timerButton: UIButton!
     
     weak var timer: Timer?
-    var number = 0
+    var timeRemaining = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +25,21 @@ final class TimerViewController: UIViewController {
     }
     
     @IBAction func sliderChanged(_ sender: UISlider) {
-        number = Int(sender.value)
-        mainLabel.text = "\(number) 초"
+        timeRemaining = Int(sender.value)
+        mainLabel.text = "\(timeRemaining) 초"
     }
     @IBAction func timerButtonTapped(_ sender: UIButton) {
-        startTimer()
+        guard let buttonTitle = sender.currentTitle else {
+            return
+        }
+        if buttonTitle == K.ButtonTitle.start ||
+            buttonTitle == K.ButtonTitle.restart {
+            intializeTimer()
+        } else {
+            stopTimer()
+        }
+        
+        toggleTimerButtonTitle(buttonTitle: buttonTitle)
     }
     @IBAction func resetButtonTapped(_ sender: UIButton) {
         configureUI()
@@ -41,24 +52,42 @@ final class TimerViewController: UIViewController {
     
     private func configureUI() {
         mainLabel.text = "초를 선택하세요"
+        timerButton.setTitle(K.ButtonTitle.start, for: .normal)
         slider.setValue(30.0, animated: true)
-        number = 0
+        timeRemaining = 0
         timer = nil
     }
     
-    private func startTimer() {
+    private func toggleTimerButtonTitle(buttonTitle: String) {
+        if buttonTitle == K.ButtonTitle.start ||
+            buttonTitle == K.ButtonTitle.restart {
+            timerButton.setTitle(K.ButtonTitle.pause, for: .normal)
+        } else {
+            timerButton.setTitle(K.ButtonTitle.restart, for: .normal)
+        }
+    }
+    
+    private func intializeTimer() {
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [self] _ in
-            if number > 0 {
-                number -= 1
-                slider.value = Float(number)
-                mainLabel.text = "\(number) 초"
-            } else {
-                timer?.invalidate()
-                number = 0
-                AudioServicesPlayAlertSound(1322)
-                configureUI()
-            }
+            startTimer()
         })
+    }
+    
+    private func startTimer() {
+        if timeRemaining > 0 {
+            timeRemaining -= 1
+            slider.value = Float(timeRemaining)
+            mainLabel.text = "\(timeRemaining) 초"
+        } else {
+            timer?.invalidate()
+            timeRemaining = 0
+            AudioServicesPlayAlertSound(1322)
+            configureUI()
+        }
+    }
+    
+    private func stopTimer() {
+        timer?.invalidate()
     }
 }
